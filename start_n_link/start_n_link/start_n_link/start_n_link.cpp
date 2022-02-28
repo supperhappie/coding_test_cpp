0050cb1563852fc00819affc86079e9302520c39
supperhappie <kwtdevel@gmail.com>
2022-02-28 17:38:47 +0900

    스타트와 링크_2

--- start_n_link/start_n_link/start_n_link/start_n_link.cpp
+++ start_n_link/start_n_link/start_n_link/start_n_link.cpp
@@ -1,16 +1,15 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
-
-unsigned char S[20][20] = { 0, };
-char m[20] = {0, };//member
 int N;
-unsigned long long ans = -1;
+int S[20][20];
+char m[20];
+unsigned int ans=-1;
 int _next = 0;
-unsigned long long getDiff() {
-	unsigned long long s = 0, l = 0;
+
+int getDiff() {
+	int s = 0, l = 0;
 	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+		for(int j=0;j<N;j++)
 			if (m[i] == m[j])
 			{
 				if (m[i] == 's')
@@ -18,47 +17,34 @@ unsigned long long getDiff() {
 				else
 					l += S[i][j];
 			}
-	if (s < l)
-		return (l - s);
-	else
-		return (s - l);
+	if (s > l)
+		return s - l;
+	else 
+		return l - s;
 }
 
-void move(int de) {
-	if (de >= N/2) {
-		unsigned long long n = getDiff();
+void move(int de){
+	if (de >= N / 2) {
+		int n = getDiff();
 		if (n < ans)
 			ans = n;
 		return;
 	}
 	for (int i = _next; i < N; i++) {
-		_next = i+1;
-		m[i] = 's';//start
+		_next = i + 1;
+		m[i] = 's';
 		move(de + 1);
 		m[i] = 'l';
 	}
 }
 
-unsigned long long game() {
-	move(0);
-	return ans;
-}
-
-int main()
-{
-#ifdef _DEBUG
-	freopen("Text.txt", "r", stdin);
-#endif
+int main() {
 	cin >> N;
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
-		{
-			int tmp;
-			cin >> tmp; 
-			S[i][j] = tmp;
-		}
-	for (int i = 0; i < N; i++)
-		m[i] = 'l';
-	cout << game() << endl;
+			cin >> S[i][j];
+	for (int i = 0; i < N; i++) m[i] = 'l';
+	move(0);
+	cout << ans << endl;
 	return 0;
 }
